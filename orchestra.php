b441f664c23964b767eae8b7f7a1cdc1d69de682
crynobone <crynobone@gmail.com>
2012-11-04 18:38:11 +0800

    Some docblock
    Signed-off-by: crynobone <crynobone@gmail.com>

--- orchestra.php
+++ orchestra.php
@@ -1,5 +1,13 @@
 <?php
-use \Config;
+
+/*
+|--------------------------------------------------------------------------
+| Register Cello
+|--------------------------------------------------------------------------
+|
+| Register Cello as Orchestra Resources.
+|
+*/
 
 Event::listen('orchestra.started', function ()
 {
@@ -11,9 +19,20 @@ Event::listen('orchestra.started', function ()
 	$cello->pages = 'cello::api.pages';
 });
 
+/*
+|--------------------------------------------------------------------------
+| Register Cello Assets for Backend
+|--------------------------------------------------------------------------
+|
+| Append all Cello required assets for Orchestra Administrator Interface usage 
+| mainly on Resources page.
+|
+*/
+
 Event::listen('orchestra.started: backend', function() 
 {
 	$asset = Asset::container('orchestra.backend');
+	
 	$asset->script('redactor', 'bundles/cello/vendor/redactor/redactor.js', array('jquery', 'bootstrap'));
 	$asset->script('cello', 'bundles/cello/js/cello.js', array('redactor'));
 	$asset->style('redactor', 'bundles/cello/vendor/redactor/css/redactor.css', array('bootstrap'));
--- start.php
+++ start.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Cello Library
+|--------------------------------------------------------------------------
+|
+| Map Cello Library using PSR-0 standard namespace. 
+*/
+
 Autoloader::namespaces(array(
 	'Cello\Model' => Bundle::path('cello').'models'.DS,
 	'Cello'       => Bundle::path('cello').'libraries'.DS,
