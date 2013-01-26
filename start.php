491df258048bee0f81af5c2d066155c656021e2c
crynobone <crynobone@gmail.com>
2013-01-26 20:54:54 +0800

    Move presenters class to it's own folder.

--- start.php
+++ start.php
@@ -10,8 +10,9 @@
 */
 
 Autoloader::namespaces(array(
-	'Cello\Model' => Bundle::path('cello').'models'.DS,
-	'Cello'       => Bundle::path('cello').'libraries'.DS,
+	'Cello\Model'     => Bundle::path('cello').'models'.DS,
+	'Cello\Presenter' => Bundle::path('cello').'presenters'.DS,
+	'Cello'           => Bundle::path('cello').'libraries'.DS,
 ));
 
 /*
