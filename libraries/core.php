61f25d143d04e225e626ac5b491de91d3eb8e299
crynobone <crynobone@gmail.com>
2013-03-08 12:13:11 +0800

    Update asset loading to be loaded in footer.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- libraries/core.php
+++ libraries/core.php
@@ -22,9 +22,9 @@ class Core {
 		// Interface usage mainly on Resources page.
 		Event::listen('orchestra.started: backend', function()
 		{
-			$asset = Asset::container('orchestra.backend');
+			$asset = Asset::container('orchestra.backend: footer');
 
-			$asset->script('redactor', 'bundles/orchestra/vendor/redactor/redactor.js', array('jquery', 'bootstrap'));
+			$asset->script('redactor', 'bundles/orchestra/vendor/redactor/redactor.min.js', array('jquery', 'bootstrap'));
 			$asset->script('cello', 'bundles/cello/js/cello.min.js', array('redactor'));
 			$asset->style('redactor', 'bundles/orchestra/vendor/redactor/css/redactor.css', array('bootstrap'));
 		});
